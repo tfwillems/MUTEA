e7abc3a010ba7af357bd9389210ab211cac67c70
Thomas Willems <twillems@club.nygenome.org>
2016-02-11 12:30:24 -0500

    Extend to read HipSTR VCFs

--- read_str_vcf.py
+++ read_str_vcf.py
@@ -1,5 +1,6 @@
 import collections
 import genotypers
+import numpy
 import operator
 import vcf
 
@@ -10,14 +11,21 @@ def get_str_read_counts(vcf_reader):
     try:
         record = vcf_reader.next()
     except StopIteration:
-        return False, "N/A", -1, -1, -1, {}, -1, -1
+        return False, "N/A", -1, -1, -1, {}, -1, -1, None
 
     # Determine most common frame
-    motif_len    = len(record.INFO['MOTIF'])
+    if 'MOTIF' in record.INFO:
+        motif_len = len(record.INFO['MOTIF'])
+    else:
+        motif_len = record.INFO['PERIOD']
     frame_counts = collections.defaultdict(int)
     for sample in record:
         if sample['GT'] is not None:
-            read_count_data  = map(lambda x: (int(x[0]), int(x[1])), map(lambda x: x.split("|"), sample['ALLREADS'].split(";")))
+            key = 'ALLREADS' if 'ALLREADS' in sample.data else 'MALLREADS'
+            if sample[key] is None:
+                continue
+
+            read_count_data  = map(lambda x: (int(x[0]), int(x[1])), map(lambda x: x.split("|"), sample[key].split(";")))
             tot_sample_reads = sum(map(lambda x: x[1], read_count_data))
             for str_length,read_count in read_count_data:
                 frame_counts[str_length%motif_len] += read_count*1.0/tot_sample_reads 
@@ -30,7 +38,9 @@ def get_str_read_counts(vcf_reader):
     in_frame_count,out_frame_count = 0,0
     for sample in record:
         if sample['GT'] is not None:
-            read_count_data = map(lambda x: (int(x[0]), int(x[1])), map(lambda x: x.split("|"), sample['ALLREADS'].split(";")))
+            if sample[key] is None:
+                continue
+            read_count_data = map(lambda x: (int(x[0]), int(x[1])), map(lambda x: x.split("|"), sample[key].split(";")))
             read_sizes      = map(lambda x: x[0], read_count_data)
             all_in_frame    = all(map(lambda x: x%motif_len == best_frame, read_sizes))
             if all_in_frame:
@@ -41,7 +51,8 @@ def get_str_read_counts(vcf_reader):
                 in_frame_count += 1
             else:
                 out_frame_count += 1
-    return True,record.CHROM,record.POS,record.INFO['END'],len(record.INFO['MOTIF']),sample_read_count_dict, in_frame_count, out_frame_count
+    str_key = record.ID if record.ID is not None else record.CHROM+":"+str(record.POS)
+    return True,record.CHROM,record.POS,record.INFO['END'],motif_len,sample_read_count_dict, in_frame_count, out_frame_count, str_key
 
 def get_str_gts(vcf_reader):
     # Attempt to read record from VCF
@@ -53,21 +64,18 @@ def get_str_gts(vcf_reader):
     # Ignore missing and heterozygous genotypes
     all_lens  = []
     for sample in record:
-        gts = sample['GT']
-        if gts is not None:
-            gt_a, gt_b = map(int, gts.split("/"))
-            if gt_a == gt_b:
-                length = len(record.alleles[gt_a])
-                all_lens.append(length)
+        if sample['GT'] is not None:
+            all_lens.append(len(str(record.alleles[int(sample['GT'])])))
 
-    # Determine most common frame 
-    motif_len    = len(record.INFO['MOTIF'])
+    # Determine most common frame
+    if 'MOTIF' in record.INFO:
+        motif_len = len(record.INFO['MOTIF'])
+    else:
+        motif_len = record.INFO['PERIOD']
     all_lens     = sorted(all_lens)
     frame_counts = motif_len*[0]
-
     if len(all_lens) == 0:
         exit("No samples have a valid genotype for the STR of interest")
-
     for length in all_lens:
         frame_counts[length%motif_len] += 1
     best_frame, value = max(enumerate(frame_counts), key=operator.itemgetter(1))
@@ -79,35 +87,43 @@ def get_str_gts(vcf_reader):
     min_str  = (all_lens[0]  - center)/motif_len
     max_str  = (all_lens[-1] - center)/motif_len
 
-    # Determine a list of alleles that are in-frame with the most common frame
-    in_frame_indices = []
-    for i in xrange(len(record.alleles)):
-        if record.alleles[i] is None:
-            continue
-        if len(record.alleles[i])%motif_len == best_frame:
-            in_frame_indices.append(i)
-
     # Determine posterior genotype likelihoods for samples
     # Samples whose most probable genotype is missing, heterozygous or out-of-frame are not assigned any likelihoods
     # Lengths of genotypes are stored relative to the 'center' and in terms of the number of repeat differences
     gt_probs  = {}
     for sample in record:
-        gts = sample['GT']
-        if gts is not None:
-            gt_a, gt_b = map(int, gts.split("/"))
-            if gt_a == gt_b and len(record.alleles[gt_a])%motif_len == best_frame:
-                # Use genotype likelihoods to assign posterior likelihoods to every homozygous in-frame STR genotype assuming a uniform genotype prior
-                PLs = sample['PL']
-                if not isinstance(PLs, list):
-                    PLs = [PLs]
-                Ls           = map(lambda x: 10**(-x/10.0), PLs)
-                homoz_Ls     = map(lambda x: Ls[(x+1)*(x+2)/2 - 1], in_frame_indices) # Annoying VCF PL indexing [(0,0), (0,1), (1,1), (0,2), (1,2), (2,2) ...]
-                tot_homoz_L  = sum(homoz_Ls)
-                sample_probs = {}
-                for index,i in enumerate(in_frame_indices): 
-                    sample_probs[(len(record.alleles[i])-center)/motif_len] = homoz_Ls[index]*1.0/tot_homoz_L
-                gt_probs[sample.sample] = sample_probs
-    return True,gt_probs,min_str,max_str,(record.CHROM+":"+str(record.POS))
+        repeat_diffs = []
+        diff_LLs     = []
+        if sample['GT'] is None:
+            continue
+        for i in xrange(len(record.alleles)):
+            if record.alleles[i] is None:
+                continue
+            if len(record.alleles[i])%motif_len != best_frame:
+                continue
+            repeat_diffs.append((len(record.alleles[i])-center)/motif_len)
+            diff_LLs.append(sample['GL'][i])
+
+        diff_LLs     = numpy.array(diff_LLs)
+        max_LL       = numpy.max(diff_LLs)
+        total_LL     = max_LL + numpy.log(numpy.sum(numpy.exp(diff_LLs-max_LL)))
+        posteriors   = numpy.exp(diff_LLs - total_LL)
+        sample_probs = {}
+
+        # Combine posteriors for repeats with same length
+        # Is there are better way than to just add them?
+        for i in xrange(len(repeat_diffs)):
+            if repeat_diffs[i] in sample_probs:
+                sample_probs[repeat_diffs[i]] += posteriors[i]
+            else:
+                sample_probs[repeat_diffs[i]] = posteriors[i]
+        gt_probs[sample.sample] = sample_probs
+
+    if record.ID is not None:
+        str_key = record.ID
+    else:
+        str_key = record.CHROM + ":" + str(record.POS)
+    return True,gt_probs,min_str,max_str,str_key
 
 def compute_median(values, counts):
     total_count = sum(counts)
